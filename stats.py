8858a1cf80a141e0b3f13e9d0554f7f601a32252
Box <contact@boxmining.com>
2020-03-23 16:00:15 +0800

    changed to default font

--- stats.py
+++ stats.py
@@ -110,11 +110,11 @@ x = 0
 
 
 # Load default font.
-#font = ImageFont.load_default()
+font = ImageFont.load_default()
 
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-font = ImageFont.truetype('04B_08__.TTF',8)
+#font = ImageFont.truetype('04B_08__.TTF',8)
 
 while True:
 
